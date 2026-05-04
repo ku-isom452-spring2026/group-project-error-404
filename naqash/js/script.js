36effa083df2ce786c748109456d184843aaad22
s2221160339 <s2221160339@ku.edu.kw>
2026-05-04 22:19:01 +0300

    added eventlistener

--- naqash/js/script.js
+++ naqash/js/script.js
@@ -1,3 +1,13 @@
-function toggleSidebar() {
-  document.getElementById("sidebar").classList.toggle("active");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  // Mobile Sidebar Toggle
+  const menuToggles = document.querySelectorAll(".menu-toggle");
+  const sidebar = document.getElementById("sidebar");
+
+  if (sidebar) {
+    menuToggles.forEach(toggle => {
+      toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("active");
+      });
+    });
+  }
+});
